a9debaf0415a8209ce8516d0bca73ef6a4d1791b
Diana Moura <di_camil@yahoo.com.br>
2021-12-10 01:32:22 -0300

    Primeira comit

---
+++ ApiDiana/Controllers/ClienteController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiDiana.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Minha Primeira API .NET CORE no Framework 5.0";
+        }
+    }
+}
